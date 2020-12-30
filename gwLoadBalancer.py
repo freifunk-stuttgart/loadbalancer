07151b1cf5b6d9400619b2d114faf5a581b9eaed
Leonard Penzer <leonard@penzer.de>
2020-12-30 15:43:01 +0100

    convert gwLoadBalancer to class

--- gwLoadBalancer.py
+++ gwLoadBalancer.py
@@ -15,168 +15,175 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-def getAvailableGwFromDns():
-    cmd = "/usr/bin/dig -t axfr gw.freifunk-stuttgart.de @dns1.lihas.de"
-    zone = subprocess.check_output(cmd.split(" ")).decode("utf-8")
-    gw = {}
-    p = re.compile('gw0[0-9]n[0-9][0-9]\.gw\.freifunk-stuttgart\.de')
-    for line in zone.split("\n"):
-        m = p.match(line)
-        if m != None:
-            gw[m.group()] = {}
-    return gw
+class GwLoadBalancer:
+    def __init__(self):
+        self.use_backbone = True
+    def getAvailableGwFromDns(self):
+        cmd = "/usr/bin/dig -t axfr gw.freifunk-stuttgart.de @dns1.lihas.de"
+        zone = subprocess.check_output(cmd.split(" ")).decode("utf-8")
+        gw = {}
+        p = re.compile('gw0[0-9]n[0-9][0-9]\.gw\.freifunk-stuttgart\.de')
+        for line in zone.split("\n"):
+            m = p.match(line)
+            if m != None:
+                gw[m.group()] = {}
+        return gw
 
-def getIpFromGwAndNum(gw,num):
-    return "10.191.255.%i%i"%(gw,num)
+    def getIpFromGwAndNum(self,gw,num):
+        return "10.191.255.%i%i"%(gw,num)
 
-def getGwAndNumFromGw(gw):
-    p = re.compile("gw([0-9]{2})n([0-9]{2})")
-    match = p.match(gw)
-    gw = int(match.group(1))
-    num = int(match.group(2))
-    return (gw,num)
+    def getGwAndNumFromGw(self,gw):
+        p = re.compile("gw([0-9]{2})n([0-9]{2})")
+        match = p.match(gw)
+        gw = int(match.group(1))
+        num = int(match.group(2))
+        return (gw,num)
 
-def getGwFromGwAndNum(gw,num):
-    return "gw%sn%s"%(str(gw).zfill(2),str(num).zfill(2))
+    def getGwFromGwAndNum(self,gw,num):
+        return "gw%sn%s"%(str(gw).zfill(2),str(num).zfill(2))
 
-def getIpFromGw(gw):
-    (gw,num) = getGwAndNumFromGw(gw)
-    return getIpFromGwAndNum(gw,num)
+    def getIpFromGw(self,gw):
+        (gw,num) = self.getGwAndNumFromGw(gw)
+        return self.getIpFromGwAndNum(gw,num)
 
-def getGwNumFromBatctlLine(line):
-    #alternative expression von are
-    #sed 's/^.*:0*\([0-9]\+\):0*\([0-9]\+\)$/10.191.255.\1\2/'    
-    p = re.compile("\ *02:[0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]([0-9]):[0-9]([0-9]).*")
-    match = p.match(line)
-    gw = int(match.group(1))
-    num = int(match.group(2))
-    return (gw,num)
+    def getGwNumFromBatctlLine(self,line):
+        #alternative expression von are
+        #sed 's/^.*:0*\([0-9]\+\):0*\([0-9]\+\)$/10.191.255.\1\2/'
+        p = re.compile("\ *02:[0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]([0-9]):[0-9]([0-9]).*")
+        match = p.match(line)
+        gw = int(match.group(1))
+        num = int(match.group(2))
+        return (gw,num)
 
-def getIpFromBatctlLine(line):
-    (gw,num) = getGwNumFromBatctlLine(line)
-    return getIpFromGwAndNum(gw,num)
+    def getIpFromBatctlLine(self,line):
+        (gw,num) = self.getGwNumFromBatctlLine(line)
+        return self.getIpFromGwAndNum(gw,num)
 
-def parseBatctlOutput(data):
-    #  02:00:38:01:07:01 (255) 02:00:35:01:07:01 [      bb01]: 64.0/64.0 MBit
-    gws = {}
-    for line in data.strip().split("\n"):
-        (gw,num) = getGwNumFromBatctlLine(line)
-        gw = getGwFromGwAndNum(gw,num)
-        gws[gw] = {}
-    return gws
+    def parseBatctlOutput(self,data):
+        #  02:00:38:01:07:01 (255) 02:00:35:01:07:01 [      bb01]: 64.0/64.0 MBit
+        gws = {}
+        for line in data.strip().split("\n"):
+            (gw,num) = self.getGwNumFromBatctlLine(line)
+            gw = self.getGwFromGwAndNum(gw,num)
+            gws[gw] = {}
+        return gws
 
-def getAvailableGwFromBatctl():
-    output = ""
-    for i in range(1,32+1):
-        seg = ("%i"%(i)).zfill(2)
-        BATCTL = "/usr/sbin/batctl"
-        if os.path.isfile(BATCTL):
-            cmd = "%s -m bat%s gwl -H -n"%(seg)
-            output += subprocess.check_output(cmd.split(" ")).decode("utf-8")
+    def getAvailableGwFromBatctl(self):
+        output = ""
+        for i in range(1,32+1):
+            seg = ("%i"%(i)).zfill(2)
+            BATCTL = "/usr/sbin/batctl"
+            if os.path.isfile(BATCTL):
+                cmd = "%s -m bat%s gwl -H -n"%(seg)
+                output += subprocess.check_output(cmd.split(" ")).decode("utf-8")
+            else:
+                with open("tests/batctl-gwl.txt","r") as fp:
+                    output += fp.read()
+        gws = self.parseBatctlOutput(output)
+        gws = self.addSelfToGws(gws)
+        return gws
+
+    def getAllStatus(self,gws):
+        for gw in gws.keys():
+            gws[gw] = self.getGwStatus(gw)
+        return gws
+
+    def getGwStatus(self,hostname):
+        if self.use_backbone:
+            fqdn = self.getIpFromGw(hostname)
         else:
-            with open("tests/batctl-gwl.txt","r") as fp:
-                output += fp.read()
-    gws = parseBatctlOutput(output)
-    gws = addSelfToGws(gws)
-    return gws
+            fqdn = "%s.gw.freifunk-stuttgart.de"%(hostname)
 
-def getAllStatus(gws):
-    for gw in gws.keys():
-        gws[gw] = getGwStatus(gw)
-    return gws
+        url = 'http://%s/data/gwstatus.json'%(fqdn)
+        try:
+            r = requests.get(url, timeout=1)
+        except Exception as e:
+            print(e)
+            return {}
 
-def getGwStatus(hostname):
-    fqdn = "%s.gw.freifunk-stuttgart.de"%(hostname)
+        if r.status_code == 404 or r.status_code == 503:
+            print("Could not get Data for %s"%(hostname))
+            return {}
 
-    url = 'http://%s/data/gwstatus.json'%(fqdn)
-    try:
-        logging.debug("Downloading stats for {}".format(hostname))
-        r = requests.get(url, timeout=1)
-    except Exception as e:
-        print(e)
-        return {}
-    
-    if r.status_code == 404 or r.status_code == 503:
-        print("Could not get Data for %s"%(hostname))
-        return {}    
-    
-    text = r.text
-    try:
-        data = json.loads(text)
-    except:
-        logging.error("Error while loading json from %s with code %i"%(hostname,r.status_code))
-        raise
-    return data
-    
+        text = r.text
+        try:
+            data = json.loads(text)
+        except:
+            print("Error while loading json from %s with code %i"%(hostname,r.status_code))
+            raise
+        return data
 
-def getPrefPerSegment(gws):
-    segments = {}
-    for gw in gws:
-        g = gws[gw]
-        if "segments" in g:
-            for s in g["segments"]:
-                if s not in segments:
-                    segments[s] = {}
-                segments[s][gw.split(".")[0]] = g["segments"][s]
-    
-    return segments
-    
-def getActiveGw(gws):
-    active = 0
-    for gw in gws:
-        if gws[gw]["dnsactive"] == 1:
-            active+=1
-    return active
 
-def getActiveGwPerSegmentFromDns():
-    cmd = "/usr/bin/dig -t axfr gw.freifunk-stuttgart.de @dns1.lihas.de"
-    zone = subprocess.check_output(cmd.split(" ")).decode("utf-8")
-    segments = {}
-    p = re.compile('gw0[0-9]s[0-6][0-9]\.gw\.freifunk-stuttgart\.de')
-    
-    for line in zone.split("\n"):
-        m = p.match(line)
-        if m != None:
-            segment = int(m.group()[5:7])
-            gw = int(m.group()[2:4])
-            #print(m.group())
-            #print(segment)
-            if not segment in segments:
-                segments[segment] = {}
-            segments[segment][gw] = None
-    return segments
+    def getPrefPerSegment(self,gws):
+        segments = {}
+        for gw in gws:
+            g = gws[gw]
+            if "segments" in g:
+                for s in g["segments"]:
+                    if s not in segments:
+                        segments[s] = {}
+                    segments[s][gw.split(".")[0]] = g["segments"][s]
 
-def decide(segments):
-    dnsActivePerSegment = getActiveGwPerSegmentFromDns()
-    for segment in segments:
-        s = segments[segment]
-        dnsActive = len(dnsActivePerSegment[int(segment)])
-        logging.debug("GWs DNS active in Segment %s: %i"%(segment,dnsActive))
-        logging.debug("In Segment %s sind %i GWs aktiv"%(segment, getActiveGw(s)))
-        logging.debug(s[localhost]["preference"])
-        preference = s[localhost]["preference"]
-        dnsactive = s[localhost]["dnsactive"]
-        if dnsactive and preference < 30:
-            logging.info("Remove this gw from DNS for segment %s"%(segment))
-        elif (preference > 35 or dnsActive < 2)and not dnsactive:
-            logging.info("Add this gw to DNS for segment %s"%(segment))
-        else:
-            pass 
-            #print("No change needed for segment %s"%(segment))
+        return segments
 
-def addSelfToGws(gws):
-    localhost = socket.gethostname()
-    if not localhost.startswith("gw"):
-        localhost = "gw01n03"
-    gws[localhost] = {}
-    return gws
+    def getActiveGw(self,gws):
+        active = 0
+        for gw in gws:
+            if gws[gw]["dnsactive"] == 1:
+                active+=1
+        return active
+
+    def getActiveGwPerSegmentFromDns(self):
+        cmd = "/usr/bin/dig -t axfr gw.freifunk-stuttgart.de @dns1.lihas.de"
+        zone = subprocess.check_output(cmd.split(" ")).decode("utf-8")
+        segments = {}
+        p = re.compile('gw0[0-9]s[0-6][0-9]\.gw\.freifunk-stuttgart\.de')
+
+        for line in zone.split("\n"):
+            m = p.match(line)
+            if m != None:
+                segment = int(m.group()[5:7])
+                gw = int(m.group()[2:4])
+                #print(m.group())
+                #print(segment)
+                if not segment in segments:
+                    segments[segment] = {}
+                segments[segment][gw] = None
+        return segments
+
+    def decide(self,segments):
+        dnsActivePerSegment = getActiveGwPerSegmentFromDns()
+        for segment in segments:
+            s = segments[segment]
+            dnsActive = len(dnsActivePerSegment[int(segment)])
+            #print("GWs DNS active in Segment %s: %i"%(segment,dnsActive))
+            #print("In Segment %s sind %i GWs aktiv"%(segment, getActiveGw(s)))
+            #print (s[localhost]["preference"])
+            preference = s[localhost]["preference"]
+            dnsactive = s[localhost]["dnsactive"]
+            if dnsactive and preference < 30:
+                print("Remove this gw from DNS for segment %s"%(segment))
+            elif (preference > 35 or dnsActive < 2)and not dnsactive:
+                print("Add this gw to DNS for segment %s"%(segment))
+            else:
+                pass
+                #print("No change needed for segment %s"%(segment))
+
+    def addSelfToGws(self,gws):
+        localhost = socket.gethostname()
+        if not localhost.startswith("gw"):
+            localhost = "gw01n03"
+        gws[localhost] = {}
+        return gws
 
 if __name__ == '__main__':
     localhost = socket.gethostname()
     if localhost == "littleblue":
         localhost = "gw01n03"
-    gws = getAvailableGwFromBatctl()
+    lb = GwLoadBalancer()
 
-    gws = getAllStatus(gws)
-    segments = getPrefPerSegment(gws)
-    decide(segments)
+    gws = lb.getAvailableGwFromBatctl()
+
+    gws = lb.getAllStatus(gws)
+    segments = lb.getPrefPerSegment(gws)
+    lb.decide(segments)
--- tests/gwLoadBalancer_tests.py
+++ tests/gwLoadBalancer_tests.py
@@ -3,50 +3,64 @@ from gwLoadBalancer import *
 
 class gwLoadBalancerTestCase(unittest.TestCase):
     def test_getIpFromGwAndNum(self):
-        ip = getIpFromGwAndNum(1,3)
+        lb = GwLoadBalancer()
+        ip = lb.getIpFromGwAndNum(1,3)
         self.assertEqual(ip,"10.191.255.13")
 
     def test_getGwAndNumFromGw(self):
-        (gw,num) = getGwAndNumFromGw("gw01n03")
+        lb = GwLoadBalancer()
+        (gw,num) = lb.getGwAndNumFromGw("gw01n03")
         self.assertEqual(gw,1)
         self.assertEqual(num,3)
 
     def test_getGwFromGwAndNum(self):
-        self.assertEqual(getGwFromGwAndNum(1,3),"gw01n03")
+        lb = GwLoadBalancer()
+        self.assertEqual(lb.getGwFromGwAndNum(1,3),"gw01n03")
+
+    def test_getIpFromGw(self):
+        lb = GwLoadBalancer()
+        self.assertEqual(lb.getIpFromGw("gw01n03"),"10.191.255.13")
 
     def test_getGwNumFromBatctlLine(self):
+        lb = GwLoadBalancer()
         line = '  02:00:38:01:07:01 (255) 02:00:35:01:07:01 [      bb01]: 64.0/64.0 MBit'
-        (gw,num) = getGwNumFromBatctlLine(line)
+        (gw,num) = lb.getGwNumFromBatctlLine(line)
         self.assertEqual(gw, 7)
         self.assertEqual(num,1)
 
     def test_getIpFromBatctlLine(self):
+        lb = GwLoadBalancer()
         line = '  02:00:38:01:07:01 (255) 02:00:35:01:07:01 [      bb01]: 64.0/64.0 MBit'
-        ip = getIpFromBatctlLine(line)
+        ip = lb.getIpFromBatctlLine(line)
         self.assertEqual(ip,"10.191.255.71")
 
     def test_parseBatctlOutput(self):
+        lb = GwLoadBalancer()
         data = """  02:00:38:01:07:01 (255) 02:00:35:01:07:01 [      bb01]: 64.0/64.0 MBit
   02:00:35:01:04:01 (255) 02:00:35:01:04:01 [      bb01]: 64.0/64.0 MBit
   02:00:35:01:05:03 (255) 02:00:35:01:05:03 [      bb01]: 64.0/64.0 MBit
   02:00:38:01:04:03 (255) 02:00:35:01:04:03 [      bb01]: 64.0/64.0 MBit
   02:00:38:01:09:02 (255) 02:00:35:01:09:02 [      bb01]: 64.0/64.0 MBit"""
-        gws = parseBatctlOutput(data)
+        gws = lb.parseBatctlOutput(data)
         #expextedGws = ["10.191.255.71","10.191.255.41","10.191.255.53","10.191.255.43","10.191.255.92"]
         expextedGws = ["gw07n01","gw04n01","gw05n03","gw04n03","gw09n02"]
         self.assertEqual(list(gws.keys()),expextedGws)
 
     def test_getAvailableGwFromBatctl(self):
-        gws = getAvailableGwFromBatctl()
+        lb = GwLoadBalancer()
+        gws = lb.getAvailableGwFromBatctl()
         #expextedGws = ["10.191.255.71", "10.191.255.41", "10.191.255.53", "10.191.255.43", "10.191.255.92"]
         expextedGws = ["gw07n01","gw04n01","gw05n03","gw04n03","gw09n02","gw01n03"]
         self.assertEqual(list(gws.keys()), expextedGws)
 
     def test_getGwStatus(self):
-        data = getGwStatus("gw01n03")
+        lb = GwLoadBalancer()
+        lb.use_backbone = False
+        data = lb.getGwStatus("gw01n03")
         self.assertEqual(data["version"],"1")
 
     def test_addSelfToGws(self):
+        lb = GwLoadBalancer()
         gws = {}
-        addSelfToGws(gws)
+        lb.addSelfToGws(gws)
         self.assertEqual(gws,{"gw01n03": {}})
