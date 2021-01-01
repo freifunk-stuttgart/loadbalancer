bca9660b05d8306b63f44e102d7c7c7e67cac85d
Leonard Penzer <leonard@penzer.de>
2021-01-01 20:32:16 +0100

    calculate nsupdate commands

--- gwLoadBalancer.py
+++ gwLoadBalancer.py
@@ -11,6 +11,8 @@ import requests
 import json
 import socket
 import os
+import sys
+import time
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -18,17 +20,25 @@ logging.basicConfig(level=logging.DEBUG)
 class GwLoadBalancer:
     def __init__(self):
         self.use_backbone = True
-        self.status = None
-    def getAvailableGwFromDns(self):
+        self.status = {}
+        self.desiredGwPerSegment = 2
+        self.allGws = {}
+        self.localhost = socket.gethostname()
+        self.segments = 32
+        self.maxAgeInSeconds = 60*15 # 15 minutes
+
+    def dnsZoneTransfer(self):
         cmd = "/usr/bin/dig -t axfr gw.freifunk-stuttgart.de @dns1.lihas.de"
-        zone = subprocess.check_output(cmd.split(" ")).decode("utf-8")
-        gw = {}
-        p = re.compile('gw0[0-9]n[0-9][0-9]\.gw\.freifunk-stuttgart\.de')
-        for line in zone.split("\n"):
+        self.zoneData = subprocess.check_output(cmd.split(" ")).decode("utf-8")
+
+    def getAvailableGwFromDns(self):
+        gw = self.allGws
+        p = re.compile('(gw0[0-9]n0[0-9])\.gw\.freifunk-stuttgart\.de')
+        for line in self.zoneData.split("\n"):
             m = p.match(line)
             if m != None:
-                gw[m.group()] = {}
-        return gw
+                gw[m.group(1)] = {}
+        self.allGws = gw
 
     def getIpFromGwAndNum(self,gw,num):
         return "10.191.255.%i%i"%(gw,num)
@@ -61,17 +71,16 @@ class GwLoadBalancer:
         return self.getIpFromGwAndNum(gw,num)
 
     def parseBatctlOutput(self,data):
+        # example:
         #  02:00:38:01:07:01 (255) 02:00:35:01:07:01 [      bb01]: 64.0/64.0 MBit
-        gws = {}
         for line in data.strip().split("\n"):
             (gw,num) = self.getGwNumFromBatctlLine(line)
             gw = self.getGwFromGwAndNum(gw,num)
-            gws[gw] = {}
-        return gws
+            self.allGws[gw] = {}
 
     def getAvailableGwFromBatctl(self):
         output = ""
-        for i in range(1,32+1):
+        for i in range(1,self.segments+1):
             seg = ("%i"%(i)).zfill(2)
             BATCTL = "/usr/sbin/batctl"
             if os.path.isfile(BATCTL):
@@ -80,16 +89,12 @@ class GwLoadBalancer:
             else:
                 with open("tests/batctl-gwl.txt","r") as fp:
                     output += fp.read()
-        gws = self.parseBatctlOutput(output)
-        gws = self.addSelfToGws(gws)
-        return gws
+        self.parseBatctlOutput(output)
+        self.addSelfToGws()
 
-    def addSelfToGws(self,gws):
-        localhost = socket.gethostname()
-        if not localhost.startswith("gw"):
-            localhost = "gw01n03"
-        gws[localhost] = {}
-        return gws
+    def addSelfToGws(self):
+        if self.localhost.startswith("gw"):
+            self.allGws[self.localhost] = {}
 
     def getGwStatusUrl(self,gw):
         if self.use_backbone:
@@ -103,96 +108,186 @@ class GwLoadBalancer:
         try:
             r = requests.get(url, timeout=1)
         except Exception as e:
-            print(e)
             return {}
-
         if r.status_code == 404 or r.status_code == 503:
             print("Could not get Data for %s" % (gw))
             return {}
         text = r.text
+        if len(text) == 0:
+            print("GW %s returns empty document"%(gw))
+            return {}
         try:
             data = json.loads(text)
-        except:
+        except Exception as e:
+            print(e)
             print("Error while loading json from %s with code %i" % (gw, r.status_code))
             raise
         return data
 
-    def getAllStatus(self,gws):
-        self.status = {}
-        for gw in gws.keys():
-            status = self.getGwStatus(gw)
-            for segment in status["segments"]:
+    def validateGwStatus(self,gw,status):
+        local_timestamp = time.time()
+        result = True
+        if (local_timestamp - status["timestamp"]) > self.maxAgeInSeconds:
+            print("Rejecting gwstatus from %s as it is too old!"%(gw))
+            result = False
+        for (segment,data) in status["segments"].items():
+            dnsactive = data["dnsactive"]
+            active = self.getActiveGwPerSegmentFromDns(segment = segment)
+            if dnsactive == 1 and gw not in active:
+                print("DNS status for %s in segment %s: Reports dnsactive==1 but is not active!"%(gw,segment))
+                result = False
+            if dnsactive == 0 and gw in active:
+                print("DNS status for %s in segment %s: Reports dnsactive==0 but is active!"%(gw,segment))
+                result = False
+        return result
+
+    def getAllStatus(self):
+        for gw in self.allGws.keys():
+            self.allGws[gw] = self.getGwStatus(gw)
+
+    def getStatus(self):
+        for (gw,gwstatus) in self.allGws.items():
+            if gwstatus == {}:
+                continue
+            if not self.validateGwStatus(gw,gwstatus):
+                gwstatus = {}
+                continue
+            for segment in gwstatus["segments"]:
                 if segment not in self.status:
                     self.status[segment] = {}
-                self.status[segment][gw] = status["segments"][segment]
+                self.status[segment][gw] = gwstatus["segments"][segment]
 
-    def printStatus(self):
-        print ("Seg | GW      | Pref")
-        for (segment,segmentdata) in self.status.items():
-            for (gw,gwdata) in segmentdata.items():
-                print("%3s | %s | %s"%(segment,gw,gwdata["preference"]))
+    def getAllGwsInSegment(self,segment):
+        segmentstatus = self.status[segment]
+        return sorted((gw for gw in segmentstatus))
 
-    def getPrefPerSegment(self,gws):
+    def getBestGwForSegment(self,segment):
+        segmentstatus = self.status[segment]
+        result = sorted(((segmentstatus[gw]["preference"],gw) for gw in segmentstatus), reverse=True)[0:self.desiredGwPerSegment]
+        return sorted(x[1] for x in result)
+
+    def getGwsWithDnsactiveEqualTo(self,segment,desiredStatus):
+        segmentstatus = self.status[segment]
+        return sorted((gw for gw in segmentstatus if segmentstatus[gw]["dnsactive"] == desiredStatus))
+
+    def getGwsThatHaveToBeAddedToDns(self,segment):
+        gwsThatHaveToBeInDns = set(self.getBestGwForSegment(segment))
+        gwsNotInDns = set(self.getGwsWithDnsactiveEqualTo(segment,0))
+        gwsThatHaveToBeAddedToDns = (gwsThatHaveToBeInDns.intersection(gwsNotInDns))
+        return sorted(gwsThatHaveToBeAddedToDns)
+
+    def getGwsThatHaveToRemovedFromDns(self,segment):
+        gwsThatHaveToBeInDns = set(self.getBestGwForSegment(segment = segment))
+        gwsInDns = set(self.getGwsWithDnsactiveEqualTo(segment = segment,desiredStatus = 1))
+        allGws = self.getAllGwsInSegment(segment = segment)
+        gwsThatHaveNotToBeInDns = set(allGws).symmetric_difference(gwsThatHaveToBeInDns).intersection(gwsInDns)
+        return sorted(gwsThatHaveNotToBeInDns)
+
+    def getResult(self):
+        result = ""
+        self.commands_all = []
+        self.commands_local = []
+        for segment in self.status:
+            gwsThatHaveToBeAddedToDns = self.getGwsThatHaveToBeAddedToDns(segment)
+            getGwsThatHaveToRemovedFromDns = self.getGwsThatHaveToRemovedFromDns(segment)
+            if len(gwsThatHaveToBeAddedToDns) > 0:
+                result += "GWs that have to be added in Segement %s to dns: %s\n"%(segment," ".join(gwsThatHaveToBeAddedToDns))
+                for gw in gwsThatHaveToBeAddedToDns:
+                    command = self.gen_nsupdate(gw,segment,"add")
+                    self.commands_all += command
+                    if gw == self.localhost:
+                        self.commands_local += command
+            if len(getGwsThatHaveToRemovedFromDns) > 0:
+                result += "GWs that have to be removed in Segement %s from dns: %s\n"%(segment," ".join(getGwsThatHaveToRemovedFromDns))
+                for gw in getGwsThatHaveToRemovedFromDns:
+                    command = self.gen_nsupdate(gw,segment,"delete")
+                    self.commands_all += command
+                    if gw == self.localhost:
+                        self.commands_local += command
+
+            if len(result) ==0:
+                result = "All is fine!"
+        return result
+
+    def getIpToGwLookup(self):
+        self.reverseDnsEntries = {}
+        p1 = re.compile('(gw0[0-9]n0[0-9])\.gw\.freifunk-stuttgart\.de\. [0-9]+ IN A*\t(.*)')
+        for line in self.zoneData.split("\n"):
+            m = p1.match(line)
+            if m != None:
+                hostname = m.group(1)
+                ip = m.group(2)
+                self.reverseDnsEntries[ip] = hostname
+
+    def getActiveGwPerSegmentFromDns(self,segment):
+        gws = []
+        zone = self.zoneData
         segments = {}
-        for gw in gws:
-            g = gws[gw]
-            if "segments" in g:
-                for s in g["segments"]:
-                    if s not in segments:
-                        segments[s] = {}
-                    segments[s][gw.split(".")[0]] = g["segments"][s]
-        return segments
-
-    def getActiveGw(self,gws):
-        active = 0
-        for gw in gws:
-            if gws[gw]["dnsactive"] == 1:
-                active+=1
-        return active
-
-    def getActiveGwPerSegmentFromDns(self):
-        cmd = "/usr/bin/dig -t axfr gw.freifunk-stuttgart.de @dns1.lihas.de"
-        zone = subprocess.check_output(cmd.split(" ")).decode("utf-8")
-        segments = {}
-        p = re.compile('gw0[0-9]s[0-6][0-9]\.gw\.freifunk-stuttgart\.de')
+        p = re.compile('(gw0[0-9])s([0-9]{2})\.gw\.freifunk-stuttgart\.de\. [0-9]+ IN A*\t(.*)')
         for line in zone.split("\n"):
             m = p.match(line)
             if m != None:
-                segment = int(m.group()[5:7])
-                gw = int(m.group()[2:4])
-                #print(m.group())
-                #print(segment)
-                if not segment in segments:
-                    segments[segment] = {}
-                segments[segment][gw] = None
-        return segments
+                s = str(int(m.group(2)))
+                gwCluster = m.group(1)
+                ip = m.group(3)
+                if s == segment and s != "99":
+                    try:
+                        gw = self.reverseDnsEntries[ip]
+                    except Exception as e:
+                        print("NO entry for GWCluster %s and IP %s"%(gwCluster,ip))
+                        raise
+                    if gw not in gws:
+                        gws.append(gw)
+        return gws
 
-    def decide(self,segments):
-        dnsActivePerSegment = getActiveGwPerSegmentFromDns()
-        for segment in segments:
-            s = segments[segment]
-            dnsActive = len(dnsActivePerSegment[int(segment)])
-            #print("GWs DNS active in Segment %s: %i"%(segment,dnsActive))
-            #print("In Segment %s sind %i GWs aktiv"%(segment, getActiveGw(s)))
-            #print (s[localhost]["preference"])
-            preference = s[localhost]["preference"]
-            dnsactive = s[localhost]["dnsactive"]
-            if dnsactive and preference < 30:
-                print("Remove this gw from DNS for segment %s"%(segment))
-            elif (preference > 35 or dnsActive < 2)and not dnsactive:
-                print("Add this gw to DNS for segment %s"%(segment))
-            else:
-                pass
-                #print("No change needed for segment %s"%(segment))
+    def validateStatus(self):
+        result = True
+        for (segment,data) in self.status.items():
+            activeGw = self.getGwsWithDnsactiveEqualTo(segment = segment, desiredStatus = 1)
+            activeGwDns = self.getActiveGwPerSegmentFromDns(segment = segment)
+            if activeGw != activeGwDns:
+                result = False
+                print("Segment %s setup is wrong: %s vs. %s"%(segment,activeGw,activeGwDns))
+        return result
+
+    def get_record_for_gw(self,gw,record):
+        p = re.compile('(gw0[0-9]n0[0-9])\.gw\.freifunk-stuttgart\.de\. [0-9]+ IN %s\t(.*)'%(record))
+        for line in self.zoneData.split("\n"):
+            m = p.match(line)
+            if m != None:
+                hostname = m.group(1)
+                ip = m.group(2)
+                if hostname == gw:
+                    return ip
+        return None
+
+    def gen_nsupdate(self,gw,segment,cmd):
+        lines = []
+        for record_type in ("A","AAAA"):
+            ip = self.get_record_for_gw(gw,record_type)
+            if ip != None:
+                s = "%ss%s"%(gw[0:4],segment.zfill(2))
+                line = "update %s %s.gw.freifunk-stuttgart.de. 300 A %s"%(cmd,s,ip)
+                lines.append(line)
+        return lines
+
+    def run(self):
+        self.dnsZoneTransfer()
+        self.getIpToGwLookup()
+        localhost = socket.gethostname()
+        if localhost.startswith("gw"):
+            self.getAvailableGwFromBatctl()
+        else:
+            self.use_backbone = False
+            self.getAvailableGwFromDns()
+        self.getAllStatus()
+        self.getStatus()
+        if not self.validateStatus():
+            print("Status is not consitent, bye!")
+            sys.exit(1)
+        report = self.getResult()
+        print(report)
 
 if __name__ == '__main__':
-    localhost = socket.gethostname()
-    if localhost == "littleblue":
-        localhost = "gw01n03"
     lb = GwLoadBalancer()
-
-    gws = lb.getAvailableGwFromBatctl()
-
-    gws = lb.getAllStatus(gws)
-    segments = lb.getPrefPerSegment(gws)
-    lb.decide(segments)
+    lb.run()
--- tests/gwLoadBalancer_tests.py
+++ tests/gwLoadBalancer_tests.py
@@ -1,7 +1,84 @@
 import unittest
+import time
 from gwLoadBalancer import *
 
 class gwLoadBalancerTestCase(unittest.TestCase):
+    def setUp(self):
+        self.gwstatus = {
+                "timestamp": 1609511049,
+                "version": "1",
+                "segments": {
+                    "1": {
+                        "preference": 33,
+                        "dnsactive": 1
+                    },
+                    "2": {
+                        "preference": 33,
+                        "dnsactive": 0
+                    }
+                }
+            }
+
+        self.gwstatus["timestamp"] = int(time.time())
+
+        self.status = {"1" : {}, "2": {}}
+        self.status["1"]["gw01n03"] = {"preference": 19, "dnsactive": 1}
+        self.status["1"]["gw04n03"] = {"preference": 22, "dnsactive": 0}
+        self.status["1"]["gw05n03"] = {"preference": 40, "dnsactive": 1}
+        self.status["1"]["gw07n01"] = {"preference": 50, "dnsactive": 1}
+        self.status["1"]["gw09n02"] = {"preference": 80, "dnsactive": 0}
+
+        self.status["2"]["gw01n03"] = {"preference": 10, "dnsactive": 0}
+        self.status["2"]["gw04n03"] = {"preference": 20, "dnsactive": 1}
+        self.status["2"]["gw05n03"] = {"preference": 30, "dnsactive": 1}
+        self.status["2"]["gw07n01"] = {"preference": 40, "dnsactive": 1}
+        self.status["2"]["gw09n02"] = {"preference": 60, "dnsactive": 1}
+
+        self.zoneData = """
+gw01n03.gw.freifunk-stuttgart.de. 300 IN A	88.198.230.6
+gw01n03.gw.freifunk-stuttgart.de. 300 IN AAAA	2a01:4f8:190:5205:260:2fff:fe08:13cd
+gw04n03.gw.freifunk-stuttgart.de. 3600 IN A	138.201.55.210
+gw04n03.gw.freifunk-stuttgart.de. 3600 IN AAAA	2a01:4f8:172:10ce::43
+gw05n03.gw.freifunk-stuttgart.de. 300 IN A	93.186.197.153
+gw05n03.gw.freifunk-stuttgart.de. 300 IN AAAA	2001:4ba0:ffff:150::1
+gw07n01.gw.freifunk-stuttgart.de. 600 IN A	163.172.12.135
+gw09n02.gw.freifunk-stuttgart.de. 600 IN A	212.227.213.45
+gw09n02.gw.freifunk-stuttgart.de. 600 IN AAAA	2001:8d8:1801:35f::92
+
+gw01s01.gw.freifunk-stuttgart.de. 300 IN AAAA	2a01:4f8:190:5205:260:2fff:fe08:13cd
+gw01s01.gw.freifunk-stuttgart.de. 300 IN A	88.198.230.6
+gw05s01.gw.freifunk-stuttgart.de. 300 IN AAAA	2001:4ba0:ffff:150::1
+gw05s01.gw.freifunk-stuttgart.de. 300 IN A	93.186.197.153
+gw07s01.gw.freifunk-stuttgart.de. 300 IN A	163.172.12.135
+
+gw04s02.gw.freifunk-stuttgart.de. 300 IN A	138.201.55.210
+gw04s02.gw.freifunk-stuttgart.de. 300 IN AAAA	2a01:4f8:172:10ce::43
+gw05s02.gw.freifunk-stuttgart.de. 300 IN AAAA	2001:4ba0:ffff:150::1
+gw05s02.gw.freifunk-stuttgart.de. 300 IN A	93.186.197.153
+gw07s02.gw.freifunk-stuttgart.de. 300 IN A	163.172.12.135
+gw09s02.gw.freifunk-stuttgart.de. 300 IN A	212.227.213.45
+gw09s02.gw.freifunk-stuttgart.de. 300 IN AAAA	2001:8d8:1801:35f::92
+"""
+
+        lb = GwLoadBalancer()
+        #all expected results assume that desiredGwPerSegment == 2
+        self.assertEqual(2,lb.desiredGwPerSegment)
+
+    def test_dnsZoneTransfer(self):
+        lb = GwLoadBalancer()
+        lb.dnsZoneTransfer()
+        self.assertIsNot("",lb.zoneData)
+
+    def test_getAvailableGwFromDns(self):
+        lb = GwLoadBalancer()
+        lb.zoneData = """gw01n03.gw.freifunk-stuttgart.de. 300 IN A	88.198.230.6
+gw01n03.gw.freifunk-stuttgart.de. 300 IN AAAA	2a01:4f8:190:5205:260:2fff:fe08:13cd
+"""
+        lb.getAvailableGwFromDns()
+        self.assertEqual({"gw01n03": {}}, lb.allGws)
+
+
+
     def test_getIpFromGwAndNum(self):
         lb = GwLoadBalancer()
         ip = lb.getIpFromGwAndNum(1,3)
@@ -44,20 +121,19 @@ class gwLoadBalancerTestCase(unittest.TestCase):
         gws = lb.parseBatctlOutput(data)
         #expextedGws = ["10.191.255.71","10.191.255.41","10.191.255.53","10.191.255.43","10.191.255.92"]
         expextedGws = ["gw07n01","gw04n01","gw05n03","gw04n03","gw09n02"]
-        self.assertEqual(list(gws.keys()),expextedGws)
+        self.assertEqual(expextedGws, list(lb.allGws.keys()))
 
     def test_getAvailableGwFromBatctl(self):
         lb = GwLoadBalancer()
-        gws = lb.getAvailableGwFromBatctl()
-        #expextedGws = ["10.191.255.71", "10.191.255.41", "10.191.255.53", "10.191.255.43", "10.191.255.92"]
-        expextedGws = ["gw07n01","gw04n01","gw05n03","gw04n03","gw09n02","gw01n03"]
-        self.assertEqual(list(gws.keys()), expextedGws)
+        lb.getAvailableGwFromBatctl()
+        expextedGws = ["gw07n01","gw04n01","gw05n03","gw04n03","gw09n02"]
+        self.assertEqual(expextedGws, list(lb.allGws.keys()))
 
     def test_addSelfToGws(self):
         lb = GwLoadBalancer()
-        gws = {}
-        lb.addSelfToGws(gws)
-        self.assertEqual(gws,{"gw01n03": {}})
+        lb.localhost = "gw01n03"
+        lb.addSelfToGws()
+        self.assertEqual({"gw01n03": {}}, lb.allGws)
 
     def test_getGwStatusUrl(self):
         lb = GwLoadBalancer()
@@ -73,16 +149,195 @@ class gwLoadBalancerTestCase(unittest.TestCase):
         data = lb.getGwStatus("gw01n03")
         self.assertEqual(data["version"],"1")
 
-    def test_getAllStatus(self):
+    def test_validateGwStatus(self):
         lb = GwLoadBalancer()
-        lb.use_backbone = False
-        lb.getAllStatus({"gw01n03": {}})
-        #self.assertEqual(lb.status["segments"]["gw01n03"]["1"]["preference"],19)
-        #self.assertIn("preference",lb.status["1"]["gw01n03"]["1"])
+        lb.zoneData = self.zoneData
+        lb.getIpToGwLookup()
 
-    def test_printStatus(self):
+        self.gwstatus["timestamp"] = int(time.time())-60*60
+        status = lb.validateGwStatus("gw01n03",self.gwstatus)
+        self.assertFalse(status)
+
+        self.gwstatus["timestamp"] = int(time.time())
+        status = lb.validateGwStatus("gw01n03",self.gwstatus)
+        self.assertTrue(status)
+
+        self.gwstatus["segments"]["1"]["dnsactive"] = 0
+        status = lb.validateGwStatus("gw01n03",self.gwstatus)
+        self.assertFalse(status)
+
+        self.gwstatus["segments"]["1"]["dnsactive"] = 1
+        self.gwstatus["segments"]["2"]["dnsactive"] = 1
+        status = lb.validateGwStatus("gw01n03",self.gwstatus)
+        self.assertFalse(status)
+
+        self.gwstatus["segments"]["1"]["dnsactive"] = 1
+        self.gwstatus["segments"]["2"]["dnsactive"] = 0
+        status = lb.validateGwStatus("gw01n03",self.gwstatus)
+        self.assertTrue(status)
+
+    def test_getAllStatus(self):
+        #this test fetches live data from gw01n03!!!
         lb = GwLoadBalancer()
         lb.use_backbone = False
-        lb.getAllStatus({"gw01n03": {}})
-        lb.status({})
-        lb.printStatus()
+        gw = "gw01n03"
+        lb.allGws = {gw: {}}
+        lb.getAllStatus()
+        self.assertIn("1",lb.allGws[gw]["segments"])
+        self.assertIn("2",lb.allGws[gw]["segments"])
+
+    def test_getStatus(self):
+        lb = GwLoadBalancer()
+        gw = "gw01n03"
+        lb.zoneData = self.zoneData
+        lb.getIpToGwLookup()
+        lb.allGws[gw] = self.gwstatus
+        lb.getStatus()
+        self.assertIn(gw,lb.status["1"])
+
+    def test_getAllGwsInSegment(self):
+        lb = GwLoadBalancer()
+        lb.status = self.status
+        all = lb.getAllGwsInSegment(segment = "1")
+        self.assertEqual(["gw01n03","gw04n03","gw05n03","gw07n01","gw09n02"],all)
+        all = lb.getAllGwsInSegment(segment = "2")
+        self.assertEqual(["gw01n03","gw04n03","gw05n03","gw07n01","gw09n02"],all)
+
+    def test_getBestGwForSegment(self):
+        lb = GwLoadBalancer()
+        lb.status = self.status
+        best = lb.getBestGwForSegment(segment = "1")
+        self.assertEqual(["gw07n01","gw09n02"],best)
+        best = lb.getBestGwForSegment(segment = "2")
+        self.assertEqual(["gw07n01","gw09n02"],best)
+
+    def test_getGwsWithDnsactiveEqualTo(self):
+        lb = GwLoadBalancer()
+        lb.status = self.status
+        active = lb.getGwsWithDnsactiveEqualTo(segment = "1", desiredStatus = 1)
+        self.assertEqual(["gw01n03","gw05n03","gw07n01"],active)
+        inactive = lb.getGwsWithDnsactiveEqualTo(segment = "1", desiredStatus = 0)
+        self.assertEqual(["gw04n03","gw09n02"],inactive)
+
+        active = lb.getGwsWithDnsactiveEqualTo(segment = "2", desiredStatus = 1)
+        self.assertEqual(["gw04n03","gw05n03","gw07n01","gw09n02"],active)
+        inactive = lb.getGwsWithDnsactiveEqualTo(segment = "2", desiredStatus = 0)
+        self.assertEqual(["gw01n03"],inactive)
+
+        active = lb.getGwsWithDnsactiveEqualTo(segment = "2", desiredStatus = 1)
+        self.assertEqual(["gw04n03","gw05n03","gw07n01","gw09n02"],active)
+        inactive = lb.getGwsWithDnsactiveEqualTo(segment = "2", desiredStatus = 0)
+        self.assertEqual(["gw01n03"],inactive)
+
+    def test_getGwsThatHaveToBeAddedToDns(self):
+        lb = GwLoadBalancer()
+        lb.status = self.status
+        gwsThatHaveToBeAdded = lb.getGwsThatHaveToBeAddedToDns(segment = "1")
+        self.assertEqual(["gw09n02"],gwsThatHaveToBeAdded)
+
+        gwsThatHaveToBeAdded = lb.getGwsThatHaveToBeAddedToDns(segment = "2")
+        self.assertEqual([],gwsThatHaveToBeAdded)
+
+    def test_getGwsThatHaveToRemovedFromDns(self):
+        lb = GwLoadBalancer()
+        lb.status = self.status
+        gwsThatHaveToBeRemoved = lb.getGwsThatHaveToRemovedFromDns(segment = "1")
+        self.assertEqual(["gw01n03","gw05n03"],gwsThatHaveToBeRemoved)
+
+        gwsThatHaveToBeRemoved = lb.getGwsThatHaveToRemovedFromDns(segment = "2")
+        self.assertEqual(["gw04n03","gw05n03"],gwsThatHaveToBeRemoved)
+
+    def test_getResult(self):
+        lb = GwLoadBalancer()
+        lb.localhost = "gw01n03"
+        lb.status = self.status
+        lb.zoneData = self.zoneData
+        report = lb.getResult()
+        expected = """GWs that have to be added in Segement 1 to dns: gw09n02
+GWs that have to be removed in Segement 1 from dns: gw01n03 gw05n03
+GWs that have to be removed in Segement 2 from dns: gw04n03 gw05n03
+"""
+        self.assertEqual(expected,report)
+        expected = [ \
+            'update add gw09s01.gw.freifunk-stuttgart.de. 300 A 212.227.213.45', \
+            'update add gw09s01.gw.freifunk-stuttgart.de. 300 A 2001:8d8:1801:35f::92', \
+            'update delete gw01s01.gw.freifunk-stuttgart.de. 300 A 88.198.230.6', \
+            'update delete gw01s01.gw.freifunk-stuttgart.de. 300 A 2a01:4f8:190:5205:260:2fff:fe08:13cd', \
+            'update delete gw05s01.gw.freifunk-stuttgart.de. 300 A 93.186.197.153', \
+            'update delete gw05s01.gw.freifunk-stuttgart.de. 300 A 2001:4ba0:ffff:150::1', \
+            'update delete gw04s02.gw.freifunk-stuttgart.de. 300 A 138.201.55.210',  \
+            'update delete gw04s02.gw.freifunk-stuttgart.de. 300 A 2a01:4f8:172:10ce::43', \
+            'update delete gw05s02.gw.freifunk-stuttgart.de. 300 A 93.186.197.153', \
+            'update delete gw05s02.gw.freifunk-stuttgart.de. 300 A 2001:4ba0:ffff:150::1']
+        for cmd in expected:
+            self.assertIn(cmd, lb.commands_all)
+        expected = \
+            [ 'update delete gw01s01.gw.freifunk-stuttgart.de. 300 A 88.198.230.6', \
+            'update delete gw01s01.gw.freifunk-stuttgart.de. 300 A 2a01:4f8:190:5205:260:2fff:fe08:13cd']
+        for cmd in expected:
+            self.assertIn(cmd, lb.commands_local)
+
+    def test_getIpToGwLookup(self):
+        lb = GwLoadBalancer()
+        lb.zoneData = self.zoneData
+        lb.getIpToGwLookup()
+        expected = \
+            {'138.201.55.210': 'gw04n03',
+             '163.172.12.135': 'gw07n01',
+             '2001:4ba0:ffff:150::1': 'gw05n03',
+             '2001:8d8:1801:35f::92': 'gw09n02',
+             '212.227.213.45': 'gw09n02',
+             '2a01:4f8:172:10ce::43': 'gw04n03',
+             '2a01:4f8:190:5205:260:2fff:fe08:13cd': 'gw01n03',
+             '88.198.230.6': 'gw01n03',
+             '93.186.197.153': 'gw05n03'}
+        self.assertEqual(expected,lb.reverseDnsEntries)
+
+    def test_getActiveGwPerSegmentFromDns(self):
+        lb = GwLoadBalancer()
+        lb.zoneData = self.zoneData
+        lb.getIpToGwLookup()
+        active = lb.getActiveGwPerSegmentFromDns(segment="1")
+        self.assertEqual(['gw01n03', 'gw05n03', 'gw07n01'],active)
+        active = lb.getActiveGwPerSegmentFromDns(segment="2")
+        self.assertEqual(['gw04n03', 'gw05n03', 'gw07n01', 'gw09n02'],active)
+
+    def test_validateStatus(self):
+        lb = GwLoadBalancer()
+        lb.zoneData = self.zoneData
+        lb.status = self.status
+        lb.getIpToGwLookup()
+
+        result = lb.validateStatus()
+        self.assertTrue(result)
+
+        lb.status["1"]["gw01n03"]["dnsactive"] = 0
+        result = lb.validateStatus()
+        self.assertFalse(result)
+
+        lb.status["1"]["gw01n03"]["dnsactive"] = 1
+        result = lb.validateStatus()
+        self.assertTrue(result)
+
+    def test_get_record_for_gw(self):
+        lb = GwLoadBalancer()
+        lb.zoneData = self.zoneData
+        gw = "gw01n03"
+        ip = lb.get_record_for_gw(gw,"A")
+        self.assertEqual("88.198.230.6",ip)
+
+        ip = lb.get_record_for_gw(gw,"AAAA")
+        self.assertEqual("2a01:4f8:190:5205:260:2fff:fe08:13cd",ip)
+
+        ip = lb.get_record_for_gw("foobar","AAAA")
+        self.assertIsNone(ip)
+
+    def test_gen_nsupdate(self):
+        lb = GwLoadBalancer()
+        lb.zoneData = self.zoneData
+        lines = lb.gen_nsupdate("gw01n03","2","add")
+        expected = \
+            ['update add gw01s02.gw.freifunk-stuttgart.de. 300 A 88.198.230.6', \
+             'update add gw01s02.gw.freifunk-stuttgart.de. 300 A 2a01:4f8:190:5205:260:2fff:fe08:13cd']
+        for entry in expected:
+            self.assertIn(entry,lines)
